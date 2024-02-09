5f80ebf2eae8f0dcd7568baba9112ebc377eef3a
Lanlinju <lanlinju@outlook.com>
2024-02-09 20:08:39 +0800

    add: 添加okhttp和retrofit依赖

--- download/build.gradle.kts
+++ download/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
 }
